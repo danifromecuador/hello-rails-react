73c3cf75054d378e7bfc43eb1016c4e759f9fcf4
dani <danifromecuador@gmail.com>
2023-10-31 14:36:08 -0500

    Add router to App

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,11 +5,23 @@ import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greetings from './components/Greetings'
+
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greetings />} />
+          <Route path="*" element={<p>Not found</p>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('root'),
 );
